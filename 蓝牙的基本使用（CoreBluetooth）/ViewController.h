8c63f2e3d80f67bc5cec3960bb942f8891317bdb
hansonboy <jianwei2013@126.com>
2016-01-04 14:43:35 +0800

    Initial Commit

---
+++ 蓝牙的基本使用（CoreBluetooth）/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  蓝牙的基本使用（CoreBluetooth）
+//
+//  Created by wangjianwei on 16/1/4.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 蓝牙的基本使用（CoreBluetooth）/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  蓝牙的基本使用（CoreBluetooth）
+//
+//  Created by wangjianwei on 16/1/4.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
